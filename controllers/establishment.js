bba6442704522a997cd38ed7ab4cc33ef46b7619
cramallo <cramallo65@gmail.com>
2019-05-30 19:56:39 -0300

    jwt validation

--- controllers/establishment.js
+++ controllers/establishment.js
@@ -2,6 +2,21 @@ const express = require('express');
 const Establishment = require('../models/establishment');
 const { validationResult } = require('express-validator/check');
 
+exports.getEstablishments = (req,res,next)=>{
+    let body = req.body;
+    Establishment.find()
+    .then(establishments=>{
+        return res.status(200).json({
+            establishments : establishments
+        });
+    })
+    .catch(error=>{
+        return res.status(500).json({
+            message:'Internal server error'
+        });
+    });
+}
+
 exports.createPost = (req,res,next)=>{
     let errors = validationResult(req);
     if(!errors.isEmpty()){
--- routes/establishment.js
+++ routes/establishment.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { body } = require('express-validator/check');
+const isAuth = require('../middlewares/isauth');
 const establishmentController = require('../controllers/establishment');
 
 const router = express.Router();
+
+router.get('/establishment',isAuth,establishmentController.getEstablishments);
 
 router.post('/establishment',[
     body('name').trim().isLength({min:1,max:10})
